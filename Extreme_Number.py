fcf4fe5741c35736aef5de4dcaba82bec2400b13
aver803bath5 <aver803bath261@gmail.com>
2016-03-23 05:02:09 -0500

    終極密碼啦

---
+++ Extreme_Number.py
@@ -0,0 +1,31 @@
+#-*-coding:utf-8-*-
+import random
+guess=0
+y=int(random.random()*99)
+
+print u"終極密碼 GO~"
+
+x=-1
+while x<0 or x>99:
+	x=input("Enter the number you guess(0~99):")
+	int(x)		
+guess+=1
+
+little=0
+big=99
+while x!=y:
+	if x>y:
+		# print y
+		big=x
+		print "Now range is %d to %d" %(little,big)
+		x=input("Enter the number you guess(0~99):")
+		int(x)
+		guess+=1
+	elif x<y:
+		# print y
+		little=x
+		print "Now range is %d to %d" %(little,big)
+		x=input("Enter the number you guess(0~99):")
+		int(x)
+		guess+=1
+print "Congratuation!!! The answer is %d, you guess %d times" %(y,guess)
