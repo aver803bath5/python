71ec6ae2edbe7fd6f13708c4f950c4e3de6420a6
aver803bath5 <aver803bath261@gmail.com>
2016-03-31 00:42:39 +0800

    花心男的 dictionary 作業

---
+++ key_flower_heart.py
@@ -0,0 +1,7 @@
+words="Honey, you are always on my mind. Your love makes me \
+blind .When I first met you I knew it was a sign. "
+
+gf={"Mary":"Honey","Lynn":"Baby","Jannie":"Little sweetie"}
+
+words=words.replace(gf["Mary"],gf["Lynn"])
+print words
